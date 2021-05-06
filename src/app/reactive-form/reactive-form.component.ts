e69dcbc87ca89b88bc521dce0b2fac8ce785c314
pavan_srinivas <170030752@kluniversity.in>
2021-05-06 19:07:27 +0530

    template

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-
+import { Validators } from '@angular/forms'
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
-  userForm = this.fb.group({
-    fullName: [''],
-    email: [''],
-    phoneNo: [''],
-    password:[''],
-    confirmPassword:[''],
-    address: [''],
-    city: [''],
-    terms: ['']
+  registrationForm = this.fb.group({
+    fullName: ['', Validators.required],
+    email: ['',[Validators.required, Validators.email]],
+    phoneNo: ['',[Validators.required, Validators.min(1000000000), Validators.max(999999999999)]],
+    password:['',[Validators.required, Validators.minLength(6)]],
+    confirmPassword:['',Validators.required, Validators.minLength(6)],
+    address: ['', Validators.required],
+    city: ['',Validators.required],
+    terms: ['',Validators.required  ]
   });
 
   constructor(private fb: FormBuilder) { }
